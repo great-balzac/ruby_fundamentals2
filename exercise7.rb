06a726d4335e1021d1d8ed8b5de3cb677aef4a3f
Daniel Pereira <pereiradpg@yahoo.ca>
2015-08-18 16:05:01 -0400

    Added steps: List gets updated with info for cohort 4 and the list is displayed.

--- exercise7.rb
+++ exercise7.rb
@@ -23,3 +23,9 @@ students = {
 # Calls method to display the current info
 # in the students hash.
 displaycohorts(students)
+
+# Add cohort 4 with a number of 43 students.
+students[:cohort4] = 43
+puts "List has been updated with numbers for cohort 4\nDisplaying updated list."
+puts ""
+displaycohorts(students)
