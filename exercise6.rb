7fe190ace117fbe4e236613a79b7068c69e24af6
Daniel Pereira <pereiradpg@yahoo.ca>
2015-08-18 15:33:00 -0400

    Added steps: Removes salmon from the grocery list and redisplays list.

--- exercise6.rb
+++ exercise6.rb
@@ -2,10 +2,13 @@
 # and prints each grocery list on it's own line
 # prefixed by an asterix.
 def displaylist(list)
+  puts "Dislpaying list"
+  puts "==============="
   list.each {
     	|item|
 	     puts "* #{item}"
     }
+  puts ""
 end
 
 # Sets grocery list.
@@ -23,14 +26,24 @@ displaylist(grocery_list)
 # bananas today."
 if grocery_list.include?("bananas")
 	puts "You need to pick up bananas."
+	puts ""
 else
 	puts "You do not need to pick up bananas today."
+	puts ""
 end
 
 # Display the second item in the list.
-puts grocery_list[1]
+puts "The second item on your grocery list today is #{grocery_list[1]}."
+puts ""
 
 # Sorts grocery list in alphabetical order
 # and outputs it again.
 grocery_list.sort!
+displaylist(grocery_list)
+
+# Can't find any salmon in the grocery store,
+# delete it from the list and redisplay.
+grocery_list.delete("salmon")
+
+puts "There was no salmon at the grocery store today, this is the\nlist without that item."
 displaylist(grocery_list)
